804b94a94ceb70706aa8d982e056d3d2501cd275
hyattjon <95375796+hyattjon@users.noreply.github.com>
2022-01-27 17:55:11 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+JSON
+/*!
+* Start Bootstrap - One Page Wonder v6.0.4 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+    function getInputFromTextBox() {
+    var email=document.getElementById("email").value;
+    alert(email);
+    }
+    
+
+    function getInputFromTextBox() {
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    alert(phoneNumber);
+    }
+
+    function getInputFromTextBox() {
+    var streetAddress = document.getElementById("address").value;
+    alert(streetAddress);
+    }
+
+    function getInputFromTextBox() {
+    var zipCode = document.getElementById("zipCode").value;
+    alert(zipCode);
+    }
